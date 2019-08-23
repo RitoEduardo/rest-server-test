42b1fcfdac0dff294815aef5010f236aa0fb3aff
Rito Eduardo García Lara <riedgala@gmail.com>
2019-08-23 18:43:36 -0500

    back init

--- server/server.js
+++ server/server.js
@@ -1,52 +1,53 @@
-
 require('../config/config');
 
-console.log("change ==>", process.env.PORT );
+console.log("change ==>", process.env.PORT);
+
+const port = process.env.PORT;
 
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const port = 3000;
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get('/', function (req, res) {
-  res.send('Hello');
-});
- 
-app.get('/users', function (req, res) {
-  res.send('Get Users');
+app.get('/', function(req, res) {
+    res.json({ init: 'Hello Word in API' });
 });
 
-app.post('/users', function (req, res) {
-  let body = req.body;
-  if( body.name === undefined ){
-    res.status(400).json({
-      resp : false,
-      msg : "The name is necesary"
+app.get('/users', function(req, res) {
+    res.json('Get Users');
+});
+
+app.post('/users', function(req, res) {
+    let body = req.body;
+    if (body.name === undefined) {
+        res.status(400).json({
+            resp: false,
+            msg: "The name is necesary"
+        });
+    }
+    res.json({
+        body,
+        resp: 'Post Users'
     });
-  }
-  res.json({
-    body,
-    resp : 'Post Users'
-  });
 });
 
-app.put('/users/:id', function (req, res) {
-  let id = req.params.id;
-  res.json({
-    id,
-    'resp' : 'Put Users'
-  });
+app.put('/users/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id,
+        'resp': 'Put Users'
+    });
 });
 
-app.delete('/users/:id', function (req, res) {
-  res.send('Delete Users');
+app.delete('/users/:id', function(req, res) {
+    res.send('Delete Users');
 });
 
-app.listen(port, () =>{
-    console.log("Init Port "+port);
+app.listen(port, () => {
+    console.log("Init Port " + port);
 });
